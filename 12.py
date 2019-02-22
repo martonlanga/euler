dce64ab0976501e80abadcdc7a7000b7fc980217
martonlanga <martonlanga@gmail.com>
2019-02-22 22:29:31 +0700

    init

---
+++ 12.py
@@ -0,0 +1,12 @@
+import time
+
+def calculate():
+    
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    result = calculate()
+    end = time.perf_counter()
+
+    print("Result:", result)
+    print(f"🏃 {end - start} 🏃")
